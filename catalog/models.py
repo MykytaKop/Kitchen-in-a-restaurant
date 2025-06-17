01462a568afa0fb147aeda4090b5a157e4fed914
admin <admin@admin.com>
2025-06-17 17:16:31 +0200

    created home page and add styles for that page

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,46 @@
+from django.urls import reverse
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.name
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("catalog:cook-detail", kwargs={"pk": self.pk})
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+    dish_type = models.ForeignKey(DishType,
+                                  on_delete=models.CASCADE,
+                                  related_name="dishes")
+    ingredients = models.ManyToManyField(Ingredient, related_name="dishes")
+
+    def __str__(self):
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,37 @@
+import generic
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from catalog.models import Cook, Dish, DishType
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_visits = request.session.get("num_visits", 1)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits,
+
+    }
+
+    return render(request, "kitchen/index.html", context=context)
+
+
+
+
+
+
