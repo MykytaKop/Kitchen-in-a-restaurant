42fb8734c14721b278f4a424e5c9979da343e51a
admin <admin@admin.com>
2025-06-21 14:18:20 +0200

    update settings and asgi file

--- kitchen_in_a_restaurant/asgi.py
+++ kitchen_in_a_restaurant/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitchen_in_a_restaurant.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kitchen_in_a_restaurant.settings.prod')
 
 application = get_asgi_application()
--- kitchen_in_a_restaurant/settings/dev.py
+++ kitchen_in_a_restaurant/settings/dev.py
@@ -4,7 +4,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1",]
 
 
 # Database
